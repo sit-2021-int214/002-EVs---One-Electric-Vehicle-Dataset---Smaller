16f8910f7c7fb1ed1bed69589bd159f57d00d292
FhLk <paramat10042545@hotmail.com>
2021-09-05 10:37:46 +0700

    Add HW1 078

---
+++ assignment/HW1/HW01_63130500078.R
@@ -0,0 +1,48 @@
+#1
+num<-c(10.4, 5.6, 3.1, 6.4, 21.7)
+num
+#Sum
+sum(num)
+#Average
+mean(num)
+#Median
+median(num)
+#Sd
+sd(num)
+#Variance
+var(num)
+
+
+#2
+# List of Marvel movies (Order by Marvel Phase released)
+names <- c("Iron Man","The Incredible Hulk","Iron Man 2","Thor","Captain America: The First Avenger",
+           "The Avengers","Iron Man 3","Thor: The Dark World","Captain America: The Winter Soldier",
+           "Guardians of the Galaxy","Avengers: Age of Ultron","Ant-Man","Captain America: Civil War",
+           "Doctor Strange","Guardians of the Galaxy 2","Spider-Man: Homecoming","Thor: Ragnarok","Black Panther",
+           "Avengers: Infinity War","Ant-Man and the Wasp","Captain Marvel","Avengers: Endgame",
+           "Spider-Man: Far From Home","WandaVision","Falcon and the Winter Soldier","Loki","Black Widow")
+
+# List of released year of Marvel movies
+years <- c(2008,2008,2010,2011,2011,2012,2013,2013,2014,2014,2015,2015,2016,2016,
+           2017,2017,2017,2017,2018,2018,2019,2019,2019,2021,2021,2021,2021)
+
+# Or using Function
+years <- c(2008,2008,2010,2011,2011,2012,rep(2013:2016,each=2),
+           rep(2017,4),rep(2018,2),rep(2019,3),rep(2021,4))
+#2.1
+#create data Frame
+marvel_movies<-data.frame(names,years)
+
+#2.2
+#Length of Names
+length(names)
+
+#The 19th movie name
+names[19]
+
+#Which year is most released movies
+data.frame(table(years))
+
+
+
+
