e45c3b8a104e89b7b3fdb8e5fd92b6dabfa7b722
FhLk <paramat10042545@hotmail.com>
2021-10-24 18:24:50 +0700

    Update hw4.R

--- assignment/HW04/63130500078/hw4.R
+++ assignment/HW04/63130500078/hw4.R
@@ -18,31 +18,31 @@ progrimming_book$Type <- progrimming_book$Type %>% as.factor()
 # what the book title have a number of Reviews more than average
 mean_review <- progrimming_book$Reviews %>% mean(.)
 reviewer <- progrimming_book %>% select(Book_title,Reviews) %>% filter(Reviews > mean_review)
-reviewer %>% View()
+reviewer %>% glimpse()
 
 # what the book title book have Type is ebook
 ebook <- progrimming_book %>% select(Book_title,Type) %>% filter(Type == "ebook") 
 
-ebook %>% View()
+ebook %>% glimpse()
 
 # what the book title have a number of page between 250 to 300
 page <- progrimming_book %>% select(Book_title,Number_Of_Pages,Price) %>% filter(between(progrimming_book$Number_Of_Pages,250,300))
-page %>% View()
+page %>% glimpse()
 
-# what book cheap and good rating
+# what book cheap and good rating(compare to average)
 mean_price <- progrimming_book$Price %>% mean(.)
 mean_rate <- progrimming_book$Rating %>% mean(.) 
 book <- progrimming_book %>% select(Book_title,Rating,Price) %>% filter(Price < mean_price & Rating > mean_rate)
-book %>% View()
+book %>% glimpse()
 
 # what the book have number of review less than 100 and have rating less than average
 reviewer100 <- progrimming_book %>% select(Book_title,Reviews,Rating) %>% filter(Reviews < 100 & Rating < mean_rate)
 
-reviewer100 %>% View()
+reviewer100 %>% glimpse()
 
-# what are the book good rate and have type is ebook
+# what are the book good rate and have type is ebook (compare with average)
 goodrate <- progrimming_book %>% select(Book_title,Rating,Type) %>% filter(Rating > mean_rate & Type =="ebook")
-goodrate %>% View()
+goodrate %>% glimpse()
 
 
 
